f9d9d054a45e7ff41ba5daac99015d9364d0d39a
ashafir <aviv@optimalfinance.co.il>
2014-10-04 18:43:14 +0300

    more tests examples

---
+++ XnitHello.Tests/RunCodeBeforeTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SampleApplication;
+using Xunit;
+
+namespace XnitHello.Tests
+{
+    public class RunCodeBeforeTests : IDisposable
+    {
+        public RunCodeBeforeTests()
+        {
+            //per-test setup code goes here
+        }
+
+        [Fact]
+        public void Decimals()
+        {
+            var d1 = new Decimal(24);
+            var d2 = new Decimal(24);
+
+            Assert.Equal(d1, d2);
+        }
+
+        public void Dispose()
+        {
+            //per-test teardown goes here
+        }
+    }
+}
---
+++ XnitHello.Tests/SkippingTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SampleApplication;
+using Xunit;
+
+namespace XnitHello.Tests
+{
+    public class SkippingTests
+    {
+        [Fact(Skip="Dont want this to run")]
+        public void InRange()
+        {
+            
+        }
+
+    }
+}
